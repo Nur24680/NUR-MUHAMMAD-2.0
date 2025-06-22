15c73c63c09fc51f2888ae8b49284e6341d2cb06
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-06-22 17:52:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require("chalk");
 const { listenMqtt } = require("facebook-chat-api");
 
 // Load appstate (cookie)
-const appState = require("../config/appstate.json");
+const appState = require("./appstate.json");
 
 listenMqtt({ appState }, async (err, api) => {
   if (err) {
