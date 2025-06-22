f9d826fea243c05071ea26bf4cfcc76f2b5f9c8d
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-06-23 00:18:39 +0600

    Update start.js

--- scripts/commands/start.js
+++ scripts/commands/start.js
@@ -1,1 +1,15 @@
-require('../index.js');
+module.exports.config = {
+  name: "start",
+  version: "1.0.0",
+  permission: 0,
+  credits: "King_Shourov",
+  description: "Check bot is alive or not",
+  prefix: true,
+  category: "utility",
+  usages: "",
+  cooldowns: 3,
+};
+
+module.exports.run = async ({ api, event }) => {
+  return api.sendMessage("✅ Shourov Bot is Alive, Running Successfully!", event.threadID, event.messageID);
+};
