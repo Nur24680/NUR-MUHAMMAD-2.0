f45ef3244568a08d55c664c8a0d8ac47d67c8580
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-06-23 22:47:20 +0600

    Create ready.js

---
+++ scripts/events/ready.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    name: "ready"
+  },
+
+  run: async ({ api }) => {
+    console.log("🤖 SHOUROV-BOT is ready!");
+    // চাইলে এখানে autostart message, group post, etc করতে পারো
+  }
+};
